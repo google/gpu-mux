7f19c37980209aa9e6f36a3355605ec105780bcc
dberth <dberth@google.com>
2018-07-24 16:22:33 -0700

    Updated the setup.py file.

--- setup.py
+++ setup.py
@@ -1,30 +1,23 @@
 import sys
 
 py_version = (sys.version_info.major, sys.version_info.minor)
-if py_version < (3, 6):
-  raise ValueError(
-    "This module is only compatible with Python 3.6+, but you are running "
-    "Python {}. We recommend installing conda and adding it to your PATH:"
-    "https://conda.io/docs/user-guide/install/index.html".format(py_version))
+if py_version < (3, 5):
+    raise ValueError(
+        "This module is only compatible with Python 3.5+, but you are running "
+        "Python {}.".format(py_version))
 
 from setuptools import setup
-import berg
 
 setup(
-  name='gpumux',
-  packages=['gpumux'],
-  version='0.0.1',
-  install_requires=[
-    "flask",
-  ],
-  author='Tom B Brown',
-  author_email='tombrown@google.com',
-  extras_require={
-    "test": [
-      'pytest ~= 3.2',
+    name='gpumux',
+    packages=['gpumux'],
+    version='1.0.0',
+    install_requires=[
+      "flask",
+    ],
+    author='David Berthelot, Tom B Brown',
+    author_email='dberth@google.com, tomfeelslucky@google.com',
+    scripts=[
+      'bin/gpumux',
     ]
-  },
-  scripts=[
-    'bin/gpumux',
-  ]
 )
